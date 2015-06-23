f94c20035d4a175f4f77b24ea10221733fe597db
averted <vitiuk.alex@gmail.com>
2015-06-23 14:29:23 -0400

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var path       = require('path')
-  , gulp       = require('gulp')
-  , uglify     = require('gulp-uglify')
-  , stylus     = require('gulp-stylus');
+var path      = require('path')
+  , gulp      = require('gulp')
+  , uglify    = require('gulp-uglify')
+  , stylus    = require('gulp-stylus')
+  , nib       = require('nib')
+  , bootstrap = require('bootstrap-styl');
 
 gulp.task('build:stylus', function() {
-  return gulp.src('styles/main.styl')
+  return gulp.src('styles/style.styl')
     .pipe(stylus({
       errors: true,
       compress: true,
